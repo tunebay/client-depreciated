1b1ee6859cf1d3a9cda2d8870b5720f3cb22f508
Mali Michael <malimccalla@gmail.com>
2017-01-13 10:46:22 +0000

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,13 @@
-var webpack = require('webpack');
 var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const VENDOR_LIBS = [
-  'react', 'redux', 'react-redux', 'redux-form',
-  'react-dom', 'redux-thunk'
-];
+// const VENDOR_LIBS = [
+//   'react', 'redux', 'react-redux', 'redux-form',
+//   'react-dom', 'redux-thunk'
+// ];
 
 module.exports = {
   entry: {
-    bundle: './src/index.js',
-    vendor: VENDOR_LIBS
+    bundle: './src/index.js'
   },
   output: {
     path: path.join(__dirname, 'dist'),
@@ -22,10 +19,6 @@ module.exports = {
         use: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
-      },
-      {
-        use: ['style-loader', 'css-loader'],
-        test: /\.css$/
       }
     ]
   }
