da096ecf753dd9a282ba06db0e50e2bd2f268225
Mali Michael <malimccalla@gmail.com>
2017-01-18 13:04:05 +0000

    Check for if user if auth'd before app renders to page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
+import { AUTH_USER } from './actions/types';
 
 import reducers from './reducers';
 import Router from './router';
@@ -10,6 +11,11 @@ import Router from './router';
 import './styles/normalize.css';
 
 const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+const token = localStorage.getItem('token');
+
+if (token) {
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
   <Provider store={store}>
