4d8e9b0e785c749c4a016742673beae6a139d436
Mali Michael <malimccalla@gmail.com>
2017-01-31 12:27:43 +0000

    style control header section

--- src/components/header/header-control.js
+++ src/components/header/header-control.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 import '../../styles/components/header/header-control.scss';
 
@@ -6,7 +7,26 @@ class HeaderControl extends Component {
   render() {
     return (
       <div className="header-control">
-        Control
+        <div className="hub">
+          <Link to="/hub" className="hub-link">Hub</Link>
+        </div>
+        <div className="dropdowns">
+          <div className="dropdown-icon">
+            <img
+              src="../../../assets/images/bell-icon.png"
+              alt="bell-icon"
+              className="bell-icon"
+            />
+          </div>
+          <div className="dropdown-icon">
+            <img
+              src="../../../assets/images/chat-icon.png"
+              alt="chat-icon"
+              className="chat-icon"
+            />
+          </div>
+          <div className="dropdown-icon"><div className="avatar" /></div>
+        </div>
       </div>
     );
   }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -23,7 +23,7 @@ class Header extends Component {
             <HeaderPlayer />
           </div>
           <div className="header-control-section">
-            <HeaderControl isAuthenticated={this.props.isAuthenticated} />
+            <HeaderControl />
           </div>
         </nav>
         <div className="progress-con">
