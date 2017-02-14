689293c35b5c936bf69270c44ee5f4ae4a63f0b2
Mali Michael <malimccalla@gmail.com>
2017-02-14 17:54:54 +0000

    More welcome styling

--- src/components/pages/welcome/jumbotron.js
+++ src/components/pages/welcome/jumbotron.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Shoutout from './shoutout';
 import '../../../styles/components/welcome/jumbotron.scss';
 
 const Jumbotron = () => {
   return (
     <div id="jumbo-con">
-      <img src="../../../assets/images/logo.png" id="jumbo-logo" alt="logo" />
+      <img src="../../../assets/images/header-logo.png" id="jumbo-logo" alt="logo" />
+      <Shoutout />
     </div>
   );
 };
