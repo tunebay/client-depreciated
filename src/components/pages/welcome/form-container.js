d69c5cfb6e895ff992fc0116f87361c84e1e042f
Mali Michael <malimccalla@gmail.com>
2017-02-15 09:07:26 +0000

    Auth welcome btns

--- src/components/pages/welcome/form-container.js
+++ src/components/pages/welcome/form-container.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
+import SignupForm from '../../auth/signup-form';
+import LoginForm from '../../auth/login-form';
 import '../../../styles/components/welcome/jumbotron.scss';
 
 class FormContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signupActive: true
+    };
+  }
+
+  handleSignupClick() {
+    if (!this.state.signupActive) {
+      this.setState({ signupActive: true });
+    }
+  }
+
+  handleLoginClick() {
+    if (this.state.signupActive) {
+      this.setState({ signupActive: false });
+    }
+  }
+
+  renderForm() {
+    if (this.state.signupActive) {
+      return <SignupForm />;
+    }
+    return <LoginForm />;
+  }
+
   render() {
     return (
       <div className="form-container">
-        <div />
-        <div />
-        <div />
+        <div className="form-con-btns">
+          <button
+            onClick={this.handleSignupClick.bind(this)}
+            className="welcome-signup-btn"
+          >Sign Up</button>
+          <button
+            onClick={this.handleLoginClick.bind(this)}
+            className="welcome-login-btn"
+          >Log In</button>
+        </div>
+        {this.renderForm()}
       </div>
     );
   }
