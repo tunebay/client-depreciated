3c51586431b123fec2a0a23788afc7be72cab67a
Mali Michael <malimccalla@gmail.com>
2017-02-23 19:39:53 +0000

    start sortable item styling

--- src/components/pages/hub/upload/upload-tracklist.js
+++ src/components/pages/hub/upload/upload-tracklist.js
@@ -4,11 +4,13 @@ import { connect } from 'react-redux';
 import { SortableContainer } from 'react-sortable-hoc';
 import * as actions from '../../../../actions/hub/upload-actions';
 import UploadedTrack from './uploaded-track';
+import '../../../../styles/components/hub/uploaded-track.scss';
+
 
 const SortableList = SortableContainer(({ tracks }) => {
   console.log('in list rendering:', tracks);
   return (
-    <ul>
+    <ul className="uploaded-track-con">
       {tracks.map((track, index) => {
         const key = v4();
         const playlistPosition = index + 1;
--- src/components/pages/hub/upload/uploaded-track.js
+++ src/components/pages/hub/upload/uploaded-track.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { SortableElement, SortableHandle } from 'react-sortable-hoc';
+import '../../../../styles/components/hub/uploaded-track.scss';
 
 const DragHandle = SortableHandle(() => <span>::</span>);
 
 const UploadedTrack = SortableElement((props) => {
   return (
-    <li>
+    <li className="uploaded-track">
       <DragHandle />
       {props.value}
     </li>
