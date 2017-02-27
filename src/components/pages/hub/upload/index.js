b9b4ea1fcf837564184d4e4f366e1bea083a136d
Mali Michael <malimccalla@gmail.com>
2017-02-27 13:16:12 +0000

    Form footer layout

--- src/components/pages/hub/upload/index.js
+++ src/components/pages/hub/upload/index.js
@@ -18,7 +18,12 @@ class HubUpload extends Component {
             <AudioUploadForm />
           </div>
           <TracklistContainer />
-          <div className="upload-form-footer"></div>
+          <div className="upload-form-footer">
+            <div className="form-footer-content">
+              <div>* Indicates required fields</div>
+              <button>Next</button>
+            </div>
+          </div>
         </UploadFormContainer>
         <p className="important-terms">
         Important: By uploading, you confirm that your audio complies with our Terms of use and you don’t infringe anyone else’s rights. If in doubt, check our Copyright information pages and FAQs before uploading.
