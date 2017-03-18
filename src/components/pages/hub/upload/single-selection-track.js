7a36e48c975030d20758e1f9b0312a1c59f3a55e
Mali Michael <malimccalla@gmail.com>
2017-03-18 22:40:43 +0000

    formatSeconds function

--- src/components/pages/hub/upload/single-selection-track.js
+++ src/components/pages/hub/upload/single-selection-track.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
-import moment from 'moment';
+import formatSeconds from '../../../../util/format-seconds';
 import '../../../../styles/components/hub/upload/single-selection-track.scss';
 
 class SingleSelectionTrack extends Component {
@@ -11,15 +11,12 @@ class SingleSelectionTrack extends Component {
       'odd-track': track.playlistPosition % 2 !== 0,
       'even-track': track.playlistPosition % 2 === 0
     });
-    console.log('LI CLASS:', liClass);
-    const formattedTime = moment('2015-01-01').startOf('day')
-    .seconds(track.duration)
-    .format('HH:mm:ss');
+
     return (
       <li className={liClass}>
         <div id="position">{track.playlistPosition}</div>
         <div id="name"><div>{track.name}</div></div>
-        <div id="time">{formattedTime}</div>
+        <div id="time">{formatSeconds(track.duration)}</div>
         <div id="single"><input type="checkbox" /></div>
         <div id="price"><input type="text" /></div>
       </li>
---
+++ src/util/format-seconds.js
@@ -0,0 +1,9 @@
+import moment from 'moment';
+
+const formatSeconds = (seconds) => {
+  return moment('1993-05-01').startOf('day').seconds(seconds)
+  .format('HH:mm:ss')
+  .replace(/^(?:00:)?0?/, '');
+};
+
+export default formatSeconds;
