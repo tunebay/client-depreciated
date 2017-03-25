7bc2b1cb2de316d2fd54826d2b3a51c66ad75369
Mali Michael <malimccalla@gmail.com>
2017-03-25 13:39:44 +0000

    Display users track in profile

--- src/components/pages/profile/tabs/playlist-display.js
+++ src/components/pages/profile/tabs/playlist-display.js
@@ -2,12 +2,22 @@ import React from 'react';
 import '../../../../styles/components/profile/music/index.scss';
 
 const PlaylistDisplay = ({ playlist }) => {
+  const tracks = playlist.tracks;
   return (
     <li className="playlist-display">
       <div className="artwork-detail-con">
         <div className="artwork" />
         <div className="detail">
           {playlist.title}
+          <ul>
+            {tracks.map((track) => {
+              return (
+                <li key={track.position}>
+                  {`${track.position}: ${track.name}`}
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
     </li>
