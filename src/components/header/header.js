64588b2bd1e76d57edef4e33079b67ae0e2a7a01
Mali Michael <malimccalla@gmail.com>
2017-03-26 12:54:59 +0100

    Header all done (kind of)

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,8 +3,6 @@ import { connect } from 'react-redux';
 import Icon from 'react-fontawesome';
 import { Link } from 'react-router';
 
-import HeaderLeft from './header-left';
-
 import '../../styles/components/header/header.scss';
 
 class Header extends Component {
@@ -13,9 +11,32 @@ class Header extends Component {
   }
 
   render() {
+    const { displayName } = this.props.currentUser;
     return (
       <nav id="top-nav">
-        <HeaderLeft />
+        <div id="header-left">
+          <div className="logo-section">
+            <div id="hamburger">
+              <Icon name="bars" size="2x" id="fa-bars" />
+            </div>
+
+            <img
+              className="logo"
+              src="../../../assets/images/logo.png"
+              alt="logo"
+            />
+            <img
+              className="logo-text"
+              src="../../../assets/images/tunebay-text.png"
+              alt="logo-text"
+            />
+          </div>
+
+          <ul className="left-menu">
+            <li><Link className="nav-link" to="/">Home</Link></li>
+            <li><Link className="nav-link" to="/discover">Discover</Link></li>
+          </ul>
+        </div>
 
         <div id="header-search">
           <div className="icon-div">
@@ -29,11 +50,16 @@ class Header extends Component {
         </div>
 
         <div id="header-right">
-          <div className="icon-div dropdown">
-            <Icon className="fa-comment" name="comment" />
+          <div className="user">
+            <Icon name="user-circle-o" size="2x" className="fa-user" />
+            <div className="display-name">{displayName}</div>
+            <Icon name="caret-down" className="fa-caret" />
           </div>
           <div className="icon-div dropdown">
             <Icon className="fa-bell" name="bell" />
+          </div>
+          <div className="icon-div dropdown">
+            <Icon className="fa-comment" name="comment" />
           </div>
           <Link className="nav-link upload-link" to="/hub">Upload</Link>
         </div>
