3bf7df38863a3b578c7095bae346550a9b1adc63
Mali Michael <malimccalla@gmail.com>
2017-03-28 12:46:06 +0100

    Convert PlaylistDisplay to container

--- src/components/pages/profile/tabs/playlist-display.js
+++ src/components/pages/profile/tabs/playlist-display.js
@@ -1,30 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PricePill from '../../../common/price-pill';
 import TrackList from './track-list';
 import SingleTrack from './single-track';
 import '../../../../styles/components/profile/music/index.scss';
 
-const PlaylistDisplay = ({ playlist }) => {
-  return (
-    <li className="playlist-display">
-      <div className="artwork-detail-con">
-        <div className="artwork" />
-        <div className="detail">
-          <div className="playlist-title-pill">
-            {playlist.title}<PricePill price={playlist.price} />
+
+class PlaylistDisplay extends Component {
+  renderTrackOrTrackList() {
+    const { tracks } = this.props.playlist;
+    return tracks.length === 1 ?
+      <SingleTrack track={tracks[0]} /> :
+      <TrackList tracks={tracks} />;
+  }
+  render() {
+    const { playlist } = this.props;
+    return (
+      <li className="playlist-display">
+        <div className="artwork-detail-con">
+          <div className="artwork" />
+          <div className="detail">
+            <div className="playlist-title-pill">
+              {playlist.title}<PricePill price={playlist.price} />
+            </div>
           </div>
         </div>
-      </div>
-      {renderTrackOrTrackList(playlist.tracks)}
-      <div className="playlist-footer" />
-    </li>
-  );
-};
-
-const renderTrackOrTrackList = (tracks) => {
-  return tracks.length === 1 ?
-    <SingleTrack track={tracks[0]} /> :
-    <TrackList tracks={tracks} />;
-};
+        {this.renderTrackOrTrackList()}
+        <div className="playlist-footer" />
+      </li>
+    );
+  }
+}
 
 export default PlaylistDisplay;
