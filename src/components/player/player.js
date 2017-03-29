bda94fabfeb35f00f0dcc97ed7d6e7be7481b7d7
Mali Michael <malimccalla@gmail.com>
2017-03-29 12:41:23 +0100

    Player styling

--- src/components/player/player.js
+++ src/components/player/player.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Sound from 'react-sound';
 import Icon from 'react-fontawesome';
 import { connect } from 'react-redux';
-import { ProgressBar } from 'react-player-controls';
+import { ProgressBar, VolumeSlider } from 'react-player-controls';
 import * as actions from '../../actions/player-actions';
 import '../../styles/components/player/player.scss';
 
@@ -50,32 +50,63 @@ class Player extends Component {
         {this.renderSound(player)}
 
         <div className="player-content">
-          <div className="now-playing">
+          <div className="player-left">
+
             <div className="player-artwork">
               <Icon name="music" size="lg" />
             </div>
 
-            <div className="now-playing-text">
-              <div className="playing-track">{player.currentTrack.name}</div>
-              <div className="playing-artist">{player.currentTrack.artist}</div>
+            <div className="playback-controls">
+              <div className="icon-div">
+                <Icon className="player-icon" name="step-backward" size="lg" />
+              </div>
+              <div className="icon-div">
+                <Icon
+                  onClick={this.togglePlayPause.bind(this)}
+                  className="player-icon"
+                  name={player.playStatus === 'PLAYING' ? 'pause' : 'play'}
+                  size="lg"
+                />
+              </div>
+              <div className="icon-div">
+                <Icon
+                  className="player-icon"
+                  name="step-forward" size="lg"
+                />
+              </div>
+              <div className="icon-div repeat-icon">
+                <Icon
+                  className="player-icon"
+                  name="repeat"
+                />
+              </div>
+            </div>
+
+            <div className="elapsed-time">
+              2:25
+            </div>
+
+          </div>
+
+          <div className="player-center">
+            <div className="playing-track">{player.currentTrack.name}</div>
+            <div className="seperator"> - </div>
+            <div className="playing-artist">{player.currentTrack.artist}</div>
+          </div>
+
+          <div className="player-right">
+            <div className="full-time">4:23</div>
+            <div className="buy">
+              <button onClick={() => console.log('buy')} className="buy-btn">
+                {`Buy ${player.currentTrack.playlistType}`}
+              </button>
+            </div>
+            <div className="volume">
+              <Icon name="volume-off" className="icon" />
+              <VolumeSlider className="VolumeSlider" volume={30} />
+              <Icon name="volume-up" className="volume-up icon" />
             </div>
           </div>
-
-          <div className="playback-controls">
-            <Icon className="player-control" name="step-backward" size="lg" />
-            <Icon
-              onClick={this.togglePlayPause.bind(this)}
-              className="player-control"
-              name={player.playStatus === 'PLAYING' ? 'pause' : 'play'}
-              size="2x"
-            />
-            <Icon
-              className="player-control"
-              name="step-forward" size="lg"
-            />
-          </div>
-
-          <div className="buy-button" />
 
         </div>
       </div>
