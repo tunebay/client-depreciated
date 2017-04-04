5c1006da9e0fb0860c08f5da6a96cd618baf2668
Mali Michael <malimccalla@gmail.com>
2017-04-04 14:59:56 +0100

    Fix few issues with home styling

--- src/components/pages/welcome/index.js
+++ src/components/pages/welcome/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Jumbotron from './jumbotron';
-// import HotRightNow from './hot-right-now';
+import HotRightNow from './hot-right-now';
 // import Layout from '../../../layout';
 
 const Welcome = () => {
   document.title = 'Tunebay | For the love of music';
   return (
-    <div>
+    <div style={{ backgroundColor: '#fff', height: 1000 }}>
       <Jumbotron />
+      <HotRightNow />
     </div>
   );
 };
