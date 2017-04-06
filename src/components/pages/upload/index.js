bdef408f7bb7a9627f7e12d1b7c80659811a43d3
Mali Michael <malimccalla@gmail.com>
2017-04-06 17:18:26 +0100

    New audio dropzone

--- src/components/pages/upload/index.js
+++ src/components/pages/upload/index.js
@@ -4,6 +4,10 @@ import Content from '../content';
 import '../../../styles/components/upload/upload-index.scss';
 
 class Upload extends Component {
+  handleDrop(files) {
+    console.log(files);
+  }
+
   handleButtonClick() {
     this.dropzone.open();
   }
@@ -14,29 +18,33 @@ class Upload extends Component {
         <div className="n-upload-container">
           <Dropzone
             ref={(node) => { this.dropzone = node; }}
+            onDrop={this.handleDrop.bind(this)}
             className="n-dropzone"
             disableClick
           >
             <div className="n-inner-drop">
-              <h1 className="n-title">
-                Drag and drop audio files here
-              </h1>
-              <div className="n-or-section">
-                <div className="n-hr" />
-                <div className="n-or">or</div>
-                <div className="n-hr" />
+              <div className="n-inner-drop-main">
+                <h1 className="n-title">
+                  Drag and drop audio files here
+                </h1>
+                <div className="n-or-section">
+                  <div className="n-hr" />
+                  <div className="n-or">or</div>
+                  <div className="n-hr" />
+                </div>
+                <button
+                  className="n-choose-files-btn"
+                  onClick={this.handleButtonClick.bind(this)}
+                >
+                  Choose files
+                </button>
               </div>
-              <button
-                className="n-choose-files-btn"
-                onClick={this.handleButtonClick.bind(this)}
-              >
-                Choose files
-              </button>
-              {/* <div className="n-upload-note">
-                Note: Drop or select multiple files if you are uploading an EP or album.
-              </div> */}
+              <div className="n-upload-note">
+                <span className="n-note">Note:</span> Select multiple files if you are uploading an EP or album.
+              </div>
             </div>
           </Dropzone>
+          <p className="n-upload-terms"><small><span className="n-important">Important:</span> By uploading, you confirm that your audio complies with our <span className="n-terms-link">Terms of Use</span> and that you are not infringing anyone else’s rights. If in doubt, read our <span className="n-terms-link">Copyright Information Page</span> and <span className="n-terms-link">FAQ’s</span> before uploading.</small></p>
         </div>
       </Content>
     );
