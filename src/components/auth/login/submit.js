4135356cfbb5e6e0b06a021018b20f7b116d3cf4
Mali Michael <malimccalla@gmail.com>
2017-04-14 08:56:31 +0100

    Fix login issue

--- src/components/auth/login/submit.js
+++ src/components/auth/login/submit.js
@@ -10,16 +10,18 @@ const submit = ({ emailOrUsername, password }, dispatch) => {
   dispatch({ type: 'VALIDATING' });
   return axios.post(`${API_URL}/login`, { emailOrUsername, password })
     .then((res) => {
-      dispatch({ type: 'FINISH_VALIDATING' });
-      dispatch({ type: 'HIDE_LOGIN_MODAL' });
-      dispatch({ type: 'AUTH_USER' });
+      console.log('setting user...');
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('user', JSON.stringify(res.data.user));
       dispatch({ type: 'SET_CURRENT_USER', payload: res.data.user });
+      dispatch({ type: 'FINISH_VALIDATING' });
+      dispatch({ type: 'HIDE_LOGIN_MODAL' });
+      dispatch({ type: 'AUTH_USER' });
+      console.log('just authd');
       console.log(res);
     })
     .catch((err) => {
-      console.log(err);
+      console.log('SUBMIT ERR,', err);
       dispatch({ type: 'FINISH_VALIDATING' });
       throw new SubmissionError({ emailOrUsername: ' ', password: 'Incorrect log in details' });
     });
