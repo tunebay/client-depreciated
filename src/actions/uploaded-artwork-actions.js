1b7f2e9d0d74ae92d21770be8ca33f13a17970db
Mali Michael <malimccalla@gmail.com>
2017-04-15 14:46:04 +0100

    Remove unused uploadImageToS3 function

--- src/actions/uploaded-artwork-actions.js
+++ src/actions/uploaded-artwork-actions.js
@@ -41,33 +41,6 @@ export const saveArtwork = (artwork) => {
 
 // private
 
-const uploadImageToS3 = (image) => {
-  return (dispatch) => {
-    // dispatch({ type: ARTWORK_UPLOAD_STARTED });
-    const filename = `users/artwork/${v4()}`;
-    axios.get(`${API_URL}/upload/s3/sign`, {
-      params: { filename, filetype: image.type }
-    })
-    .then((res) => {
-      const config = {
-        headers: { 'Content-Type': image.type },
-      };
-      return axios.put(res.data.signedURL, image, config);
-    })
-    .then((res) => {
-      console.log('ARTWORK RES', res);
-      const location = res.config.url.split('?')[0];
-      console.log('IMAGE LOCATION', location);
-      // dispatch({ type: ARTWORK_UPLOAD_COMPLETE, payload: location });
-    })
-    .catch((err) => {
-      console.log('UPLOAD ERROR', err);
-      // dispatch({ type: ARTWORK_UPLOAD_ERROR, payload: err });
-    });
-  };
-};
-
-
 const dataURLtoBlob = (dataurl) => {
   var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
     bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
