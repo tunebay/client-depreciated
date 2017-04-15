a11b48e12500a2f79259caa9849deadd64f46be8
Mali Michael <malimccalla@gmail.com>
2017-04-15 13:21:35 +0100

    Progress with canvas

--- src/components/pages/upload/artwork/artwork-modal.js
+++ src/components/pages/upload/artwork/artwork-modal.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Modal from 'react-modal';
-// import AvatarEditor from 'react-avatar-editor';
+import AvatarEditor from 'react-avatar-editor';
 import '../../../../styles/components/upload/upload-artwork-modal.scss';
 
-const ArtworkModal = ({ isVisable, requestCloseFn, preview }) => {
-  return (
-    <Modal
-      isOpen={isVisable}
-      className="artwork-modal"
-      overlayClassName="overlay"
-      contentLabel="artworkModal"
-      shouldCloseOnOverlayClick
-      onRequestClose={requestCloseFn}
-    >
-      <div id="canvas-mask">
-        <img src={preview} className="artwork-preview" alt="artwork-preview" />
-      </div>
-    </Modal>
-  );
-};
+class ArtworkModal extends Component {
+  getImage() {
+    const artwork = this.editor.getImage();
+    console.log(artwork);
+  }
+
+  render() {
+    const { isVisable, requestCloseFn, preview } = this.props;
+    return (
+      <Modal
+        isOpen={isVisable}
+        className="artwork-modal"
+        overlayClassName="overlay"
+        contentLabel="artworkModal"
+        shouldCloseOnOverlayClick
+        onRequestClose={requestCloseFn}
+      >
+        <AvatarEditor
+          ref={(node) => { this.editor = node; }}
+          image={preview}
+          width={480}
+          style={{ width: 480, height: 480 }}
+          height={480}
+          border={0}
+          color={[255, 255, 255, 0.6]} // RGBA
+          scale={1.2}
+          rotate={0}
+        />
+        <button onClick={this.getImage.bind(this)}>
+          Get image
+        </button>
+      </Modal>
+    );
+  }
+}
 
 export default ArtworkModal;
