a31f8bedad27a22f85a3e1d2e918da10876f554a
Mali Michael <malimccalla@gmail.com>
2017-04-23 22:03:51 +0100

    Smooootthh fucking progress barrss say whaaaaaaaatt

--- src/components/pages/upload/uploaded-single.js
+++ src/components/pages/upload/uploaded-single.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Line as Progress } from 'rc-progress';
+// import { Line as Progress } from 'rc-progress';
+import { Line } from 'react-progressbar.js';
+
 import '../../../styles/components/upload/uploaded-single.scss';
 
 class UploadedSingle extends Component {
   render() {
     const track = this.props.playlist[0];
     console.log(track.progress);
-    return (
-      <Progress
-        className="progress"
-        percent={track.progress}
-        strokeWidth={0.5}
-        strokeColor={'#0089EE'}
-        trailColor={'#e4e4e4'}
-        strokeLinecap="square"
-      />
-    );
+    var options = {
+               strokeWidth: 0.8
+           };
+
+           // For demo purposes so the container has some dimensions.
+           // Otherwise progress bar won't be shown
+           var containerStyle = {
+               width: '100%',
+           };
+
+           return (
+               <Line
+                   progress={track.progress / 100 }
+                   options={options}
+                   initialAnimate={true}
+                   containerStyle={containerStyle}
+                   containerClassName={'.progressbar'} />
+           );
   }
 }
 
