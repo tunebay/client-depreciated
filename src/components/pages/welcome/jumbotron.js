078e6b933ee82956194447b3d593f8a5db6d8e8a
Mali Michael <malimccalla@gmail.com>
2017-05-04 14:23:50 +0100

    Fix jumbo styling

--- src/components/pages/welcome/jumbotron.js
+++ src/components/pages/welcome/jumbotron.js
@@ -17,8 +17,8 @@ const Jumbotron = (props) => {
       <div className="overlay">
 
         <div className="shout-out">
-          <h1 className="title">For the love of music!</h1>
-          <p className="tag">Connect with a directly support the artists and music you love.</p>
+          <h1 className="title">{'For the love of music!'}</h1>
+          <p className="tag">{'Connect with & directly support the music you love.'}</p>
           <div className="action-buttons">
             <Link className="discover-btn" to="/discover">Discover</Link>
             <Link className="sell-your-own-btn" to="/">Sell your own</Link>
