b0311dd3ff4902c9566298da8a3f3fba551d1f8d
Mali Michael <malimccalla@gmail.com>
2017-05-17 11:32:15 +0100

    update eslint and add shims

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'es6-shim';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import './styles/normalize.css';
