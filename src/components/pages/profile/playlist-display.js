bedcf543605119f70cfdb7cb97704dac23c434d8
Mali Michael <malimccalla@gmail.com>
2017-06-19 15:20:07 +0100

    Rename artwork and artwork-con classes

--- src/components/pages/profile/playlist-display.js
+++ src/components/pages/profile/playlist-display.js
@@ -3,8 +3,8 @@ import React from 'react';
 const PlaylistDisplay = ({ playlist }) => {
   return (
     <li className="playlist-display">
-      <div className="artwork-con">
-        <img className="artwork" src={playlist.artwork} alt="artwork" />
+      <div className="artwork">
+        <img className="artwork-image" src={playlist.artwork} alt="artwork" />
       </div>
       <div className="playlist-details">
         <div>{playlist.title}</div>
