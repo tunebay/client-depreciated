9ed0ded73e72ba7bb74b8a04842ba909d3da5c52
Mali Michael <malimccalla@gmail.com>
2017-06-23 16:30:48 +0100

    Starts building bones of playlist display player

--- src/components/pages/profile/playlist-display.js
+++ src/components/pages/profile/playlist-display.js
@@ -2,14 +2,34 @@ import React from 'react';
 import Artwork from './artwork';
 import PricePill from '../../common/price-pill';
 
-const PlaylistDisplay = ({ playlist }) => {
+const PlaylistDisplay = ({ playlist: { price, tracks, title, artwork } }) => {
+  console.log('tracks', tracks);
   return (
     <li className="playlist-display">
-      <Artwork artwork={playlist.artwork} title={playlist.title} />
+      <Artwork artwork={artwork} title={title} />
       <div className="playlist-details">
         <div className="title">
-          {playlist.title} <PricePill price={playlist.price} />
+          {title} <PricePill price={price} />
         </div>
+        <div className="playlist-player">
+          <button className="play-btn">
+            <img
+              src="../../../../assets/images/triangle.svg"
+              alt="play"
+              className="play"
+            />
+          </button>
+          <div className="track-to-play">
+            <div className="track-title">
+              {`${tracks[0].playlistPosition}. ${tracks[0].name}`}
+            </div>
+            <div className="playlist-player-title">
+              {title}
+            </div>
+          </div>
+        </div>
+        <div className="waveform" />
+        <div className="playlist-footer" />
       </div>
     </li>
   );
