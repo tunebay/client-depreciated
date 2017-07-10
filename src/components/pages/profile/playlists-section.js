0e4b228e5247184299decf1a57a4026950d4226d
Mali Michael <malimccalla@gmail.com>
2017-07-10 13:45:05 +0100

    No playlists display

--- src/components/pages/profile/playlists-section.js
+++ src/components/pages/profile/playlists-section.js
@@ -5,9 +5,13 @@ import './styles/playlists-section.scss';
 class PlaylistsSection extends Component {
   render() {
     const { playlists } = this.props;
+    console.log(playlists.length === 0);
     return (
       <div id="playlists-section">
-        <UserPlaylists playlists={playlists} />
+        {playlists.length !== 0 ?
+          <UserPlaylists playlists={playlists} /> :
+          <div className="no-playlists">Nothing to hear here, this user hasnt uploaded any music yet.</div>
+        }
       </div>
     );
   }
