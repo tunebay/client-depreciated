0e0a1b9f2f35a938332cf4ed98020651348c3db8
Mali Michael <malimccalla@gmail.com>
2017-07-25 09:51:42 +0100

    Active profile NavLink

--- src/components/pages/profile/menu-nav.js
+++ src/components/pages/profile/menu-nav.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './styles/menu-nav.scss';
 
 class MenuNav extends Component {
@@ -7,9 +7,9 @@ class MenuNav extends Component {
     const { playlistCount, username } = this.props;
     return (
       <ul id="menu-nav">
-        <Link to={`/${username}`} className="menu-nav-item active">Music &nbsp;({playlistCount})</Link>
-        <Link to={`/${username}/videos`} className="menu-nav-item">Videos</Link>
-        <Link to={`/${username}/collection`} className="menu-nav-item">Collection</Link>
+        <NavLink to={`/${username}`} exact className="menu-nav-item">Music &nbsp;({playlistCount})</NavLink>
+        <NavLink to={`/${username}/videos`} className="menu-nav-item">Videos</NavLink>
+        <NavLink to={`/${username}/collection`} className="menu-nav-item">Collection</NavLink>
       </ul>
     );
   }
