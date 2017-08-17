6c315067b3327508b6de168fd419c1c3ce3c40c4
Mali Michael <malimccalla@gmail.com>
2017-08-17 12:20:48 +0100

    Jumbotron fix

--- src/components/pages/welcome/jumbotron.js
+++ src/components/pages/welcome/jumbotron.js
@@ -9,45 +9,81 @@ const Jumbotron = (props) => {
   return (
     <div id="jumbo-con">
       <div className="logo-con">
-        <img src="../../../../assets/images/logo.svg" alt="logo" className="logo" />
-        <img src="../../../../assets/images/tunebay-light.svg" alt="logo" className="tunebay" />
+        <img
+          src="../../../../assets/images/logo.svg"
+          alt="logo"
+          className="logo"
+        />
+        <img
+          src="../../../../assets/images/tunebay-light.svg"
+          alt="logo"
+          className="tunebay"
+        />
       </div>
       <div className="overlay">
-
         <div className="shout-out">
-          <h1 className="title">{'For the love of music!'}</h1>
-          <h2 className="tag">{'Connect with & directly support the music you love.'}</h2>
+          <h1 className="title">
+            {'For the love of music!'}
+          </h1>
+          <h2 className="tag">
+            {'Directly support the music & artists you love.'}
+          </h2>
           <div className="action-buttons">
-            <Link className="discover-btn" to="/discover">Discover</Link>
-            <Link className="sell-your-own-btn" to="/">Sell Your Own</Link>
+            <Link className="discover-btn" to="/discover">
+              Discover
+            </Link>
+            <Link className="sell-your-own-btn" to="/">
+              Sell Your Own
+            </Link>
           </div>
         </div>
         <div className="auth-container">
           <div className="auth-con-header">
-            <button onClick={showLoginModal} className="login-btn">Log In</button>
+            <button onClick={showLoginModal} className="login-btn">
+              Log In
+            </button>
           </div>
           <div className="auth-content">
             <h2 className="auth-title">Create an account</h2>
-            <p className="auth-tag">Join the fastest growing community of music makers and music lovers today.</p>
+            <p className="auth-tag">
+              Join the fastest growing community of music makers and music
+              lovers today.
+            </p>
             <button className="fb-btn">
               <Icon className="fb-logo" name="facebook" size="lg" />
-              <div className="btn-txt">
-                Continue with Facebook
-              </div>
+              <div className="btn-txt">Continue with Facebook</div>
             </button>
             <button className="google-btn">
-              <img className="google-logo" src="../../../../assets/images/g.svg" alt="google" />
-              <div className="btn-txt">
-                Continue with Google
-              </div>
+              <img
+                className="google-logo"
+                src="../../../../assets/images/g.svg"
+                alt="google"
+              />
+              <div className="btn-txt">Continue with Google</div>
             </button>
             <div className="or">
               <div className="hr" />
               or
               <div className="hr" />
             </div>
-            <Link className="signup-email-btn" to="/signup">Continue with Email</Link>
-            <p className="terms"><small>By signing up, I agree to Tunebay’s <Link className="terms-link" to="/terms" target="_blank">Terms of Service</Link>, <Link className="terms-link" to="/terms">Payments Terms of Service</Link> and <Link className="terms-link" to="/terms">Privacy Policy</Link>.</small></p>
+            <Link className="signup-email-btn" to="/signup">
+              Continue with Email
+            </Link>
+            <p className="terms">
+              <small>
+                By signing up, I agree to Tunebay’s{' '}
+                <Link className="terms-link" to="/terms" target="_blank">
+                  Terms of Service
+                </Link>,{' '}
+                <Link className="terms-link" to="/terms">
+                  Payments Terms of Service
+                </Link>{' '}
+                and{' '}
+                <Link className="terms-link" to="/terms">
+                  Privacy Policy
+                </Link>.
+              </small>
+            </p>
           </div>
         </div>
       </div>
